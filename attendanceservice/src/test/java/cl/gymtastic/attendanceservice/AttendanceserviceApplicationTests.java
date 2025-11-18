09d51309ffb2bd163d4bab682f2bbcb673ab39a2
Ivan Hernandez <iva.hernandezl@duocuc.cl>
2025-11-18 15:15:18 -0300

    attendance

---
+++ attendanceservice/src/test/java/cl/gymtastic/attendanceservice/AttendanceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.gymtastic.attendanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
