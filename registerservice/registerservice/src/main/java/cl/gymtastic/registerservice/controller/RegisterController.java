89bb8c4af335f2a12865086ab0637c2a02a0fe99
Ivan Hernandez <iva.hernandezl@duocuc.cl>
2025-11-26 08:40:54 -0300

    crossorigin

--- registerservice/registerservice/src/main/java/cl/gymtastic/registerservice/controller/RegisterController.java
+++ registerservice/registerservice/src/main/java/cl/gymtastic/registerservice/controller/RegisterController.java
@@ -1,23 +1,22 @@
 package cl.gymtastic.registerservice.controller;
 
 import cl.gymtastic.registerservice.dto.RegisterRequest;
-// UserResponse ya no se usa aquí
 import cl.gymtastic.registerservice.service.RegisterService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity; // Importar
-import org.springframework.web.bind.annotation.*;
-import java.util.Map; // Importar
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*; // Importa CrossOrigin aquí
+import java.util.Map;
 
 @RestController
 @RequestMapping("/register")
 @RequiredArgsConstructor
+@CrossOrigin // <--- ¡IMPORTANTE! Agrega esto para permitir la conexión con el Frontend
 public class RegisterController {
 
     private final RegisterService registerService;
 
     @PostMapping
-    // CORREGIDO: Tipo de retorno
     public ResponseEntity<Map<String, String>> register(@Valid @RequestBody RegisterRequest request) {
         return registerService.register(request);
     }
